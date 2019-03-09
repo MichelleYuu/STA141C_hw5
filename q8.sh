63a292868658f6f43eb9f9de2dd46210e073f066
s141c account 42 <s141c-42@gauss.cse.ucdavis.edu>
2019-03-09 12:07:41 -0800

    Adding solutions

--- q8.sh
+++ q8.sh
@@ -1,2 +1,12 @@
-cat transaction_new.csv | Rscript tester4.R
-
+# remove last row because it is the column headers
+   sed '$d' transaction_new.csv |
+   cat > transaction_new2.csv
+ 
+   # get the first digit
+   cut -d',' -f2 transaction_new2.csv |
+   cut -c-1 |
+   cat > first_digit.csv
+ 
+   # combine with parent_ID
+   paste -d"," transaction_new2.csv first_digit.csv |
+   cut -d"," -f3,4 > ID_digit.csv
